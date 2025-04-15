2455572c2dbb21997b3f86e4d3f3dfb96374e33f
met80227 <meta.thurid.lotz@isi.fraunhofer.de>
2025-04-15 17:16:17 +0200

    differentiation of historical and future flows for cement model

--- src/cement_topdown/cement_topdown_mfa_system.py
+++ src/cement_topdown/cement_topdown_mfa_system.py
@@ -12,64 +12,66 @@ class CementTopdownMFASystem(fd.MFASystem):
         flw = self.flows
         stk = self.stocks
 
-        flw["Cement production => Cement market"][...] = prm["cement_production"]
-        flw["Clinker market => Cement production"][...] = flw["Cement production => Cement market"] * \
-            prm["clinker_factor"]
-        flw["Clinker production => Clinker market"][...] = flw["Clinker market => Cement production"][...] - \
-            prm["trade_clinker"]
-        flw["Cement market => Concrete production"][...] = flw["Cement production => Cement market"][...] - \
-            prm["trade_cement"]
-        flw["Concrete production => Concrete market"][...] = flw["Cement market => Concrete production"][...] / \
-            prm["cement_to_concrete"]
-        flw["Concrete market => End use stock historic"][...] = (flw["Concrete production => Concrete market"][...] - \
-            prm["trade_concrete"]) * prm["end_use_matrix"]
-        stk["End use stock historic"].inflow[...] = flw["Concrete market => End use stock historic"][...]
+        flw["Cement production historic => Cement market historic"][...] = prm["cement_production"]
+        flw["Clinker market historic => Cement production historic"][...] = \
+            flw["Cement production historic => Cement market historic"] * prm["clinker_factor"]
+        flw["Clinker production historic => Clinker market historic"][...] = \
+            flw["Clinker market historic => Cement production historic"][...] - prm["trade_clinker"]
+        flw["Cement market historic => Concrete production historic"][...] = \
+            flw["Cement production historic => Cement market historic"][...] - prm["trade_cement"]
+        flw["Concrete production historic => Concrete market historic"][...] = \
+            flw["Cement market historic => Concrete production historic"][...] / prm["cement_to_concrete"]
+        flw["Concrete market historic => End use stock historic"][...] = \
+            (flw["Concrete production historic => Concrete market historic"][...] -
+             prm["trade_concrete"]) * prm["end_use_matrix"]
+        stk["End use stock historic"].inflow[...] = \
+            flw["Concrete market historic => End use stock historic"][...]
         stk["End use stock historic"].lifetime_model.set_prms(
             mean=prm["end_use_lifetime_mean"], std=prm["end_use_lifetime_std"]
         )
         stk["End use stock historic"].compute()
-        flw["End use stock historic => CDW collection"][...] = stk["End use stock historic"].outflow * \
+        flw["End use stock historic => CDW collection historic"][...] = stk["End use stock historic"].outflow * \
             prm["dissipative_losses"]
-        flw["CDW collection => CDW unsorted market"][...] = flw["End use stock historic => CDW collection"][...] * \
-            prm["mapping_waste"]
-        flw["CDW unsorted market => CDW separation"][...] = flw["CDW collection => CDW unsorted market"][...] - \
-            prm["trade_CDW_unsorted"]
-        flw["CDW separation => CDW sorted market"][...] = flw["CDW unsorted market => CDW separation"][...] * \
-            prm["separation_efficiency"]
-        flw["CDW sorted market => sysenv"][...] = flw["CDW separation => CDW sorted market"][...] - \
-            prm["trade_CDW_sorted"]
+        flw["CDW collection historic => CDW unsorted market historic"][...] = \
+            flw["End use stock historic => CDW collection historic"][...] * prm["mapping_waste"]
+        flw["CDW unsorted market historic => CDW separation historic"][...] = \
+            flw["CDW collection historic => CDW unsorted market historic"][...] - prm["trade_CDW_unsorted"]
+        flw["CDW separation historic => CDW sorted market historic"][...] = \
+            flw["CDW unsorted market historic => CDW separation historic"][...] * prm["separation_efficiency"]
+        flw["CDW sorted market historic => sysenv historic"][...] = \
+            flw["CDW separation historic => CDW sorted market historic"][...] - prm["trade_CDW_sorted"]
 
     def compute_future_stock(self):
         prm = self.parameters
         flw = self.flows
         stk = self.stocks
 
-        flw["Concrete market => End use stock future"][...] = prm["start_value"] * prm["growth_rate"]
-        flw["Concrete production => Concrete market"][...] = \
-            flw["Concrete market => End use stock future"].sum_to(("t", "j", "f")) - prm["trade_concrete"]
-        flw["Cement market => Concrete production"][...]= flw["Concrete production => Concrete market"] * \
-            prm["cement_to_concrete"]
-        flw["Cement production => Cement market"][...] = flw["Cement market => Concrete production"] - \
-            prm["trade_cement"]
-        flw["Clinker market => Cement production"][...] = flw["Cement production => Cement market"] * \
-            prm["trade_clinker"]
-        flw["Clinker production => Clinker market"][...] = flw["Clinker market => Cement production"] - \
-            prm["trade_clinker"]
-        stk["End use stock future"].inflow[...] = flw["Concrete market => End use stock future"][...]
+        flw["Concrete market future => End use stock future"][...] = prm["start_value"] * prm["growth_rate"]
+        flw["Concrete production future => Concrete market future"][...] = \
+            flw["Concrete market future => End use stock future"].sum_to(("t", "j", "f")) - prm["trade_concrete"]
+        flw["Cement market future => Concrete production future"][...]= \
+            flw["Concrete production future => Concrete market future"] * prm["cement_to_concrete"]
+        flw["Cement production future => Cement market future"][...] = \
+            flw["Cement market future => Concrete production future"] - prm["trade_cement"]
+        flw["Clinker market future => Cement production future"][...] = \
+            flw["Cement production future => Cement market future"] * prm["trade_clinker"]
+        flw["Clinker production future => Clinker market future"][...] = \
+            flw["Clinker market future => Cement production future"] - prm["trade_clinker"]
+        stk["End use stock future"].inflow[...] = flw["Concrete market future => End use stock future"][...]
         stk["End use stock future"].lifetime_model.set_prms(
             mean=prm["end_use_lifetime_mean"], std=prm["end_use_lifetime_std"]
         )
         stk["End use stock future"].compute()
-        flw["End use stock future => CDW collection"][...] = stk["End use stock future"].outflow * \
+        flw["End use stock future => CDW collection future"][...] = stk["End use stock future"].outflow * \
                                                                prm["dissipative_losses"]
-        flw["CDW collection => CDW unsorted market"][...] = flw["End use stock future => CDW collection"][...] * \
-                                                            prm["mapping_waste"]
-        flw["CDW unsorted market => CDW separation"][...] = flw["CDW collection => CDW unsorted market"][...] - \
-                                                            prm["trade_CDW_unsorted"]
-        flw["CDW separation => CDW sorted market"][...] = flw["CDW unsorted market => CDW separation"][...] * \
-                                                          prm["separation_efficiency"]
-        flw["CDW sorted market => sysenv"][...] = flw["CDW separation => CDW sorted market"][...] - \
-                                                  prm["trade_CDW_sorted"]
+        flw["CDW collection future => CDW unsorted market future"][...] = \
+            flw["End use stock future => CDW collection future"][...] * prm["mapping_waste"]
+        flw["CDW unsorted market future => CDW separation future"][...] = \
+            flw["CDW collection future => CDW unsorted market future"][...] - prm["trade_CDW_unsorted"]
+        flw["CDW separation future => CDW sorted market future"][...] = \
+            flw["CDW unsorted market future => CDW separation future"][...] * prm["separation_efficiency"]
+        flw["CDW sorted market future => sysenv future"][...] = \
+            flw["CDW separation future => CDW sorted market future"][...] - prm["trade_CDW_sorted"]
 
 
     def get_flows_as_dataframes(self):
