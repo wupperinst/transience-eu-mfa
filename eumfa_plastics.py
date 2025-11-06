5520c5a80a1185b9f30c12206568c6e8e5f826c3
Mathieu Saurat <mathieu.saurat@ensta.org>
2025-11-06 15:24:25 +0100

    Implement passing scenario name as argument to eumfa_plastics.py

--- eumfa_plastics.py
+++ eumfa_plastics.py
@@ -1,8 +1,21 @@
+import argparse
+import logging
 from run_eumfa import run_eumfa
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
+
 #logging.basicConfig(level=logging.INFO)
 
-cfg_file = "config/plastics.yml"
+# ARGUMENTS
+parser = argparse.ArgumentParser(description='Get scenario for plastics sub-module.')
+parser.add_argument('-s', '--scenario', dest='scenario', type=str, help='scenario names')
+args = parser.parse_args()
+
+# Scenario name
+if args.scenario is None:
+    scenario = 'baseline'
+else:
+    scenario = args.scenario
+
+# Run EUMFA with the specified scenario
+cfg_file = f"config/plastics_{scenario}.yml"
 run_eumfa(cfg_file)
